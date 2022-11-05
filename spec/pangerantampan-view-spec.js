212e0c8b22174b81765774d93efbccefd1145219
pangerantampan <irenevaleda89@gmail.com>
2022-11-06 02:20:26 +0800

    tanpan

---
+++ spec/pangerantampan-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PangerantampanView from '../lib/pangerantampan-view';
+
+describe('PangerantampanView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
